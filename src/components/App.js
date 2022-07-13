1e8741233c07bcae91b4b24cb209956c2daff144
root <root@DESKTOP-KRL36LH.localdomain>
2022-07-13 17:05:57 +0530

    done

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
+const App = () => {
+  const [adviceObj, setAdviceObj] = useState({ slip: {} });
+  const [isFetched, setIsFetched] = useState(false);
+
+  const changeAdvice = () => {
+   
+  };
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <div id="main">
+      <div className="container">
+        <h1>Advice </h1>
+        <p id="advice"></p>
+        <div className="svg">
+          <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
+            <g fill="none" fillRule="evenodd">
+              <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
+              <g transform="translate(212)" fill="#CEE3E9">
+                <rect width="6" height="16" rx="3" />
+                <rect x="14" width="6" height="16" rx="3" />
+              </g>
+            </g>
+          </svg>
+        </div>
+        <div className="icon" onClick={changeAdvice} id="dice">
+          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
+              fill="#202733"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
